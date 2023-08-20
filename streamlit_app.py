bb04cfdedc77d4721accac9622f5c046c86a7899
raphacb <91844553+raphacb@users.noreply.github.com>
2023-08-20 16:58:03 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,132 +1,87 @@
-# Desenvolvimento de Aplicativo de Gerenciamento de Dados Laboratoriais
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Documentação e análise de dados de testes de antibiograma</title>
+</head>
+<body>
+  <h1>Documentação e análise de dados de testes de antibiograma</h1>
 
-# Título: Desenvolvimento de Aplicativo de Gerenciamento de Dados Laboratoriais
-# Descrição: Crie um aplicativo inteligente que facilite a coleta, armazenamento, visualização e análise de dados laboratoriais. O aplicativo é composto por um formulário intuitivo para entrada de dados e um gráfico dinâmico para visualização dos resultados. Ele é especialmente projetado para monitorar o crescimento de bactérias ao longo do tempo, mas também pode ser personalizado para atender às necessidades específicas de diferentes laboratórios.
+  <form action="/teste" method="post">
+    <input type="text" name="data" placeholder="Data">
+    <input type="time" name="hora" placeholder="Hora">
+    <input type="text" name="local" placeholder="Local">
+    <input type="text" name="tipo_amostra" placeholder="Tipo de amostra">
+    <input type="text" name="antibiotico" placeholder="Antibiótico">
+    <input type="number" name="concentracao" placeholder="Concentração">
+    <input type="number" name="crescimento" placeholder="Crescimento">
+    <input type="submit" value="Enviar">
+  </form>
 
-# Funcionalidades Principais:
-# 1. Formulário de Entrada de Dados:
-#    - O aplicativo apresenta um formulário de fácil utilização para a coleta de dados.
-#    - Os campos obrigatórios incluem: Data, Hora, Local, Tipo de Amostra e Antibiótico.
-#    - Também oferece a possibilidade de adicionar campos personalizados, garantindo flexibilidade.
+  <div id="graficos"></div>
 
-# 2. Visualização de Dados:
-#    - Os dados coletados são armazenados em formato JSON e usados para gerar um gráfico de linha.
-#    - O gráfico exibe o crescimento de bactérias em relação ao tempo, permitindo a análise de tendências.
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/chart.js/3.7.0/chart.min.js"></script>
 
-# 3. Opções de Filtro:
-#    - Os usuários podem filtrar os dados com base em critérios como data, hora, local, tipo de amostra ou antibiótico.
+  <script>
+    $(document).ready(function() {
+      // Obtém os dados do formulário
+      var data = $("#teste").serializeArray();
 
-# 4. Exportação de Dados:
-#    - Os dados podem ser exportados para formatos CSV ou PDF, facilitando a documentação e compartilhamento.
+      // Cria um objeto JSON com os dados
+      var dados = JSON.stringify(data);
 
-# 5. Correlação de Informações:
-#    - Ferramentas estão disponíveis para identificar correlações entre informações de diferentes testes, auxiliando na análise.
-
-# 7. Ampliação para Outros Tipos de Dados:
-#    - O aplicativo pode ser expandido para incluir mais campos de dados, como resultados de testes de sensibilidade a antibióticos, perfil de resistência bacteriana e dados clínicos.
-
-# 8. Personalização:
-#    - Um painel de configurações permite personalizar o aplicativo para atender às necessidades específicas de cada laboratório.
-#    - Usuários podem adicionar campos personalizados, ajustar o layout do formulário e dos gráficos, e definir opções de exportação.
-
-# Benefícios:
-#    - Facilita o processo de coleta, armazenamento e análise de dados laboratoriais.
-#    - O gráfico dinâmico ajuda na identificação de padrões e tendências.
-#    - A possibilidade de ampliação para diferentes tipos de dados aumenta a versatilidade.
-#    - A personalização garante que o aplicativo atenda às necessidades únicas de cada laboratório.
-
-# Código Python:
-
-import json
-
-class Laboratorio:
-    def __init__(self):
-        self.dados = []
-
-    def coletar_dados(self, data, hora, local, tipo_amostra, antibiotico, **kwargs):
-        novo_dado = {
-            "Data": data,
-            "Hora": hora,
-            "Local": local,
-            "Tipo de Amostra": tipo_amostra,
-            "Antibiótico": antibiotico
+      // Carrega os dados no gráfico
+      var ctx = document.getElementById("graficos").getContext("2d");
+      var chart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: data.map(function(item) {
+            return item.data;
+          }),
+          datasets: [{
+            label: "Crescimento de bactérias",
+            data: data.map(function(item) {
+              return item.value;
+            }),
+            backgroundColor: "rgba(255, 0, 0, 0.2)",
+            borderColor: "rgb(255, 0, 0)",
+            borderWidth: 1
+          }]
+        },
+        options: {
+          title: "Crescimento de bactérias em função do tempo",
+          scales: {
+            y: {
+              min: 0,
+              max: 100
+            }
+          }
         }
-        novo_dado.update(kwargs)
-        self.dados.append(novo_dado)
+      });
 
-    def filtrar_dados(self, **kwargs):
-        resultados = []
-        for dado in self.dados:
-            if all(key in dado and dado[key] == value for key, value in kwargs.items()):
-                resultados.append(dado)
-        return resultados
+      // Adiciona opções de filtro
+      var filter = document.querySelector(".filter");
+      filter.addEventListener("change", function() {
+        var filtro = filter.value;
+        var dadosFiltrados = dados.filter(function(item) {
+          return item[filtro] === true;
+        });
+        chart.data.datasets[0].data = dadosFiltrados.map(function(item) {
+          return item.value;
+        });
+        chart.update();
+      });
 
-    def exportar_dados_csv(self, nome_arquivo):
-        with open(nome_arquivo, "w") as arquivo:
-            arquivo.write("Data,Hora,Local,Tipo de Amostra,Antibiótico\n")
-            for dado in self.dados:
-                linha = f"{dado['Data']},{dado['Hora']},{dado['Local']},{dado['Tipo de Amostra']},{dado['Antibiótico']}\n"
-                arquivo.write(linha)
-
-    def exportar_dados_json(self, nome_arquivo):
-        with open(nome_arquivo, "w") as arquivo:
-            json.dump(self.dados, arquivo)
-
-    def gerar_grafico(self):
-        # Código para gerar o gráfico de linha
-        pass
-
-    def identificar_correlacoes(self):
-        # Código para identificar correlações entre informações de diferentes testes
-        pass
-
-    def adicionar_campo_personalizado(self, nome_campo):
-        # Código para adicionar campo personalizado ao formulário
-        pass
-
-    def ajustar_layout_formulario(self):
-        # Código para ajustar o layout do formulário
-        pass
-
-    def ajustar_layout_grafico(self):
-        # Código para ajustar o layout do gráfico
-        pass
-
-    def definir_opcoes_exportacao(self):
-        # Código para definir opções de exportação
-        pass
-
-# Exemplo de uso:
-
-laboratorio = Laboratorio()
-
-# Coletar dados
-laboratorio.coletar_dados("2021-01-01", "09:00", "Laboratório A", "Sangue", "Penicilina", Resultado="Positivo")
-laboratorio.coletar_dados("2021-01-02", "10:30", "Laboratório B", "Urina", "Cefalosporina", Resultado="Negativo")
-
-# Filtrar dados
-resultados = laboratorio.filtrar_dados(Local="Laboratório A", Antibiótico="Penicilina")
-for resultado in resultados:
-    print(resultado)
-
-# Exportar dados
-laboratorio.exportar_dados_csv("dados_laboratorio.csv")
-laboratorio.exportar_dados_json("dados_laboratorio.json")
-
-# Gerar gráfico
-laboratorio.gerar_grafico()
-
-# Identificar correlações
-laboratorio.identificar_correlacoes()
-
-# Adicionar campo personalizado
-laboratorio.adicionar_campo_personalizado("Resultado")
-
-# Ajustar layout do formulário
-laboratorio.ajustar_layout_formulario()
-
-# Ajustar layout do gráfico
-laboratorio.ajustar_layout_grafico()
-
-# Definir opções de exportação
-laboratorio.definir_opcoes_exportacao()
+      // Adiciona botões de exportação
+      var exportarCSV = document.querySelector(".exportar-csv");
+      exportarCSV.addEventListener("click", function() {
+        var dadosExportados = JSON.parse(dados);
+        window.location.href = "/exportar/csv?dados=" + encodeURIComponent(JSON.stringify(dadosExportados));
+      });
+      var exportarPDF = document.querySelector(".exportar-pdf");
+      exportarPDF.addEventListener("click", function() {
+        var dadosExportados = JSON.parse(dados);
+        window.location.href = "/exportar/pdf?dados=" + encodeURIComponent(JSON.stringify(dadosExportados));
+      });
+    });
+  </script>
